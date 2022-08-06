16395e3884461c2ee21b1de89d944b986c77e9f7
AbdulRafay099 <96542884+AbdulRafay099@users.noreply.github.com>
2022-08-06 10:44:33 +0400

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApniShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+      
+        public int Demand { get; set; }
+        public byte[] Image { get; set; }
+        [Range(1, 5, ErrorMessage = "Rating must be between 1-5.")]
+        public int Rating { get; set; }
+
+    }
+}
